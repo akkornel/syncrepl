d802d4410b98726bbe94b8c647aab567ea6da3fa
A. Karl Kornel <karl@kornel.us>
2017-05-25 20:51:13 -0700

    Create setup script

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# vim: sw=4 ts=4 et
+
+# syncrepl_client installer code.
+#
+# Refer to the AUTHORS file for copyright statements.
+#
+# This file contains only factual information.
+# Therefore, this file is likely not copyrightable.
+# As such, this file is in the public domain.
+# For locations where public domain does not exist, this file is licensed
+# under the Creative Commons CC0 Public Domain Dedication.
+
+from distutils.core import setup
+
+setup(
+    version = '0.75',
+
+    name = 'syncrepl-client',
+    description = 'An easier-to-use LDAP syncrepl client',
+
+    author = 'A. Karl Kornel',
+    author_email = 'karl@kornel.us',
+
+    url = 'http://github.com/akkornel/syncrepl',
+
+    package_dir = { '': 'lib' },
+
+    requires = [
+        'python-ldap>2.4.38',
+    ],
+
+    license = 'BSD 3-Clause',
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP',
+    ]
+)
