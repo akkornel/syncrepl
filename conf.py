bc83a0f0db0ac35b6f0096be412305560c57c78b
A. Karl Kornel <akkornel@stanford.edu>
2017-05-26 15:23:49 -0700

    Set default Sphinx domain

--- conf.py
+++ conf.py
@@ -80,6 +80,10 @@ release = '0.75'
 # Usually you set "language" from the command line for these cases.
 language = None
 
+# Set the default domain to "py" (Python), which is the default, but
+# let's make that clear!
+default_domain = 'py'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
