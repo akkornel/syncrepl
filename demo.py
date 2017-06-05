6989ae15da4c8edd0eba81b18579e4410f016383
A. Karl Kornel <akkornel@stanford.edu>
2017-06-05 13:51:13 -0700

    Clarify that docstrings are CC BY-SA 4.0 licensed
    The docstrings are processed into documentation using Sphinx.  Therefore,
    it can be said that the Sphinx-generated docs are derivatives of the
    source.
    
    My intention is for the docs to be licensed under CC BY-SA 4.0.  So, that
    means the docstrings should also be licensed under CC BY-SA 4.0, in
    addition to the BSD 3-Clause license which covers the rest of the code.

--- demo.py
+++ demo.py
@@ -6,30 +6,17 @@
 #
 # Refer to the AUTHORS file for copyright statements.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
+# This file is made available under the terms of the BSD 3-Clause License,
+# the text of which may be found in the file `LICENSE.md` that was included
+# with this distribution, and also at
+# https://github.com/akkornel/syncrepl/blob/master/LICENSE.md 
 #
-# * Redistributions of source code must retain the above copyright notice, this
-#   list of conditions and the following disclaimer.
+# The Python docstrings contained in this file are also made available under the terms
+# of the Creative Commons Attribution-ShareAlike 4.0 International Public License,
+# the text of which may be found in the file `LICENSE_others.md` that was included
+# with this distribution, and also at
+# https://github.com/akkornel/syncrepl/blob/master/LICENSE_others.md
 #
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-#
-# * Neither the name of the copyright holder nor the names of its
-#   contributors may be used to endorse or promote products derived from
-#   this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 from syncrepl_client import Syncrepl, SyncreplMode
