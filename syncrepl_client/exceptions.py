6f157804ef3286fb1ae621e31e948aff13b16584
A. Karl Kornel <akkornel@stanford.edu>
2017-06-10 22:36:36 -0700

    Clean up exceptions definitions
    Create a few new exceptions, and clean up documentation on the ones we already
    have.
    
    Related to #8

--- syncrepl_client/exceptions.py
+++ syncrepl_client/exceptions.py
@@ -19,11 +19,66 @@
 #
 
 
-# For Python 2 support
-from __future__ import print_function
+class ClosedError(Exception):
+    """Action performed on an unbound instance.
 
+    This exception is thrown when a call is made to an instance which has been
+    unbound.
 
-class LDAPUrlError(Exception):
+    If you wish to reconnect to the server, you must use a new instance.
+    """
     pass
 
 
+class LDAPUrlError(Exception):
+    """Generic LDAP URL-related exception.
+
+    All of the exceptions related to LDAP URLs are subclasses of this
+    exception.
+    """
+    pass
+
+
+class LDAPUrlConflict(LDAPUrlError):
+    """Thrown when a URL conflicts with an existing URL.
+
+    This exception is thrown when a data store already has a URL, and the
+    client provided a new LDAP URL that conflicts.
+
+    The following attributes should be checked:
+
+    * The search base DN.
+
+    * The search scope.
+
+    * The search filter.
+
+    * The list of attributes to return.
+
+    If any of the above attributes conflict, this exception is thrown.
+
+    Attributes:
+
+        current_url -- An :class:`~ldapurl.LDAPUrl` object; the current URL.
+
+        new_url -- An :class:`~ldapurl.LDAPUrl` object; the new, conflicting URL.
+    """
+
+    def __init__(self, current_url, new_url):
+        self.current_url = current_url
+        self.new_url = new_url
+
+
+class LDAPUrlParseError(LDAPUrlError):
+    """Thrown when an LDAP URL can not be parsed.
+
+    This exception is thrown when an LDAP URL string is provided, but that
+    string can not be parsed.
+
+    Attributes:
+
+        url -- The URL string provided by the client.
+    """
+
+    def __init__(self, url):
+        self.url = url
