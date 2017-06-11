551db887b5cda207bef34806d8fa36b5b1ee7819
A. Karl Kornel <akkornel@stanford.edu>
2017-06-10 21:22:54 -0700

    Update classifiers with Python versions

--- setup.py
+++ setup.py
@@ -76,6 +76,11 @@ setup(
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP'
     ]
 )
